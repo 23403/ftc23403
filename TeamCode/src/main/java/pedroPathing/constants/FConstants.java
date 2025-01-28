7f0eb559b12eece8edc5e0a918b4588ee97bbb68
23403 <grieasdavid@gmail.com>
2025-01-28 18:02:33 -0500

    odometry

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -20,22 +20,22 @@ public class FConstants {
 
         FollowerConstants.mass = 10.43;
 
-        FollowerConstants.xMovement = 51.49793125622908;
+        FollowerConstants.xMovement = 49.547294817807064;
         FollowerConstants.yMovement = 37.08035531803594;
 
         FollowerConstants.forwardZeroPowerAcceleration = -63.92373053702034;
         FollowerConstants.lateralZeroPowerAcceleration = -43.34666291381834;
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
-        FollowerConstants.useSecondaryTranslationalPID = false;
+        FollowerConstants.useSecondaryTranslationalPID = true;
         FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
 
         FollowerConstants.headingPIDFCoefficients.setCoefficients(2,0,0.1,0);
-        FollowerConstants.useSecondaryHeadingPID = false;
+        FollowerConstants.useSecondaryHeadingPID = true;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
         FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
-        FollowerConstants.useSecondaryDrivePID = false;
+        FollowerConstants.useSecondaryDrivePID = true;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
         FollowerConstants.zeroPowerAccelerationMultiplier = 4;
