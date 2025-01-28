0e7254f008e8294c6a197b51233a0aec30edb511
NIN1275 <106284474+nin1275@users.noreply.github.com>
2025-01-27 20:41:04 -0500

    odometry localizer tuned

---
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -0,0 +1,24 @@
+package pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+public class LConstants {
+    static {
+        TwoWheelConstants.forwardTicksToInches = -0.0163;
+        TwoWheelConstants.strafeTicksToInches = 0.01376;
+        TwoWheelConstants.forwardY = -6;
+        TwoWheelConstants.strafeX = 4;
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightRear";
+        TwoWheelConstants.forwardEncoderDirection = Encoder.FORWARD;
+        TwoWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
+        TwoWheelConstants.IMU_HardwareMapName = "imu";
+        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
+    }
+}
+
+
+
+
