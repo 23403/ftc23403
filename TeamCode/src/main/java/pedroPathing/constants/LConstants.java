ecf765ca18ea9f25c5006aa498985e57499e05f1
23403 <grieasdavid@gmail.com>
2025-02-05 15:45:36 -0500

    odometry

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -6,12 +6,12 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class LConstants {
     static {
-        TwoWheelConstants.forwardTicksToInches = 0.03333333333;
-        TwoWheelConstants.strafeTicksToInches = 0.0348;
+        TwoWheelConstants.forwardTicksToInches = 0.00198302872;
+        TwoWheelConstants.strafeTicksToInches = -0.00199300074;
         TwoWheelConstants.forwardY = -6;
         TwoWheelConstants.strafeX = 4;
         TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
-        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightRear";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
         TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
         TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
         TwoWheelConstants.IMU_HardwareMapName = "imu";
