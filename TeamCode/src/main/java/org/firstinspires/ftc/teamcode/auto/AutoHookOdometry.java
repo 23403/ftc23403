9253dd63711bd314da9fa1b48984277d32706eac
NIN1275 <106284474+nin1275@users.noreply.github.com>
2025-02-07 22:55:53 -0500

    added, changed, and removed comments to make sense or to inform myself :sob:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoHookOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoHookOdometry.java
@@ -18,22 +18,21 @@ import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
 /**
- * This is an example auto that showcases movement and control of two servos autonomously.
- * It is a 0+4 (Specimen + Sample) bucket auto. It scores a neutral preload and then pickups 3 samples from the ground and scores them before parking.
- * There are examples of different ways to build paths.
- * A path progression method has been created and can advance based on time, position, or other factors.
- *
- * @author Baron Henderson - 20077 The Indubitables
- * @version 2.0, 11/28/2024
+ * Code Conductor's first auto with odometry.
+ * Started code  @  2/6/25  @  5:37 pm
+ * Finished code  @  2/7/25  @  10:49 pm
+ * 2 hours each day
+ * It is a 1+0 specimen auto. It hangs a preloaded specimen and then push the 3 samples from the ground and park.
+ * @author David Grieas - 23403 Code Conductors
+ * @version 1.2, 2/7/25
  */
 
 @Autonomous(name = "Auto Hook", group = ".ftc23403")
 public class AutoHookOdometry extends OpMode {
     private Follower follower;
-    private Timer pathTimer, actionTimer, opmodeTimer;
+    private Timer pathTimer, opmodeTimer;
 
-    /** This is the variable where we store the state of our auto.
-     * It is used by the pathUpdate method. */
+    /** store the state of our auto. */
     private int pathState;
 
     /* Create and Define Poses + Paths
@@ -41,11 +40,10 @@ public class AutoHookOdometry extends OpMode {
      * Pedro uses 0 - 144 for x and y, with 0, 0 being on the bottom left.
      * (For Into the Deep, this would be Blue Observation Zone (0,0) to Red Observation Zone (144,144).)
      * Even though Pedro uses a different coordinate system than RR, you can convert any roadrunner pose by adding +72 both the x and y.
-     * This visualizer is very easy to use to find and create paths/pathchains/poses: <https://pedro-path-generator.vercel.app/>
-     * Lets assume our robot is 18 by 18 inches
-     * Lets assume the Robot is facing the human player and we want to score in the bucket */
+     * This visualizer is very easy to use to find and create paths/pathChains/poses: <https://pedro-path-generator.vercel.app/>
+     */
 
-    /** Start Pose of our robot */
+    /** line positions */
     private final Pose startPos = new Pose(9, 56, Math.toRadians(0)); // start Pos
     private final Pose moveForward = new Pose(29, 56, Math.toRadians(0)); // line1
     private final Pose specimenLoc = new Pose(33, 71, Math.toRadians(0)); // line2
@@ -60,13 +58,13 @@ public class AutoHookOdometry extends OpMode {
     private final Pose goBack1 = new Pose(62, 16, Math.toRadians(0)); // line10
     private final Pose moveSide2 = new Pose(62, 14.5, Math.toRadians(0)); // line11
     private final Pose pushBott2 = new Pose(16, 13.5, Math.toRadians(0)); // line12
+    /** different modes */
     private Path scorePreload;
+    /** path name */
     private PathChain hook, hook1, goYellow, pushBot1, pushBot2, pushBot3, pushBot4, pushBot5, pushBot6, pushBot7, pushBot8, pushBot9;
 
-    /** Build the paths for the auto (adds, for example, constant/linear headings while doing paths)
-     * It is necessary to do this so that all the paths are built before the auto starts. **/
+    /** create paths */
     public void buildPaths() {
-
         /* There are two major types of paths components: BezierCurves and BezierLines.
          *    * BezierCurves are curved, and require >= 3 points. There are the start and end points, and the control points.
          *    - Control points manipulate the curve between the start and end points.
@@ -136,9 +134,6 @@ public class AutoHookOdometry extends OpMode {
                 .build();
     }
 
-    /** This switch is called continuously and runs the pathing, at certain points, it triggers the action state.
-     * Everytime the switch changes case, it will reset the timer. (This is because of the setPathState() method)
-     * The followPath() function sets the follower to run the specific path, but does NOT wait for it to finish before moving on. */
     /* You could check for
         - Follower State: "if(!follower.isBusy() {}"
         - Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
@@ -151,11 +146,9 @@ public class AutoHookOdometry extends OpMode {
                 setPathState(1);
                 break;
             case 1:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 turnArmMove(775, 1);
                 extendArmMove(451, 447, 1);
                 if(!follower.isBusy()) {
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(hook,true);
                     setPathState(2);
                 }
@@ -168,111 +161,67 @@ public class AutoHookOdometry extends OpMode {
                     setPathState(3);
                 }
             case 3:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position */
                 if(!follower.isBusy()) {
-                    /* Grab Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     follower.followPath(goYellow,true);
                     setPathState(4);
                 }
                 break;
             case 4:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(pushBot1,true);
                     setPathState(5);
                 }
                 break;
             case 5:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup2Pose's position */
                 if(!follower.isBusy()) {
-                    /* Grab Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     follower.followPath(pushBot2,true);
                     setPathState(6);
                 }
                 break;
             case 6:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(pushBot3,true);
                     setPathState(7);
                 }
                 break;
             case 7:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
                 if(!follower.isBusy()) {
-                    /* Grab Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     follower.followPath(pushBot4, true);
                     setPathState(8);
                 }
                 break;
             case 8:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are parked */
                     follower.followPath(pushBot5,true);
                     setPathState(9);
                 }
                 break;
             case 9:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are parked */
                     follower.followPath(pushBot6,true);
                     setPathState(10);
                 }
                 break;
             case 10:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are parked */
                     follower.followPath(pushBot7,true);
                     setPathState(11);
                 }
                 break;
             case 11:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are parked */
                     follower.followPath(pushBot8,true);
                     setPathState(12);
                 }
                 break;
             case 12:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are parked */
                     follower.followPath(pushBot9,true);
                     setPathState(13);
                 }
                 break;
             case 13:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Level 1 Ascent */
-
-                    /* Set the state to a Case we won't use or define, so it just stops running an new paths */
                     setPathState(-1);
                 }
                 break;
